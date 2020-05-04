1898367e7e65f061e178861e43a8b60354838bed
WONILLISM <wonillism@gmail.com>
2020-05-04 21:17:28 +0900

    Solve 04 v02

--- 03.programmers/2019_kakao_winter_internship/01.C++/04.cpp
+++ 03.programmers/2019_kakao_winter_internship/01.C++/04.cpp
@@ -9,17 +9,12 @@
 
 
 using namespace std;
-
-/*
-1. 한 번에 한 명씩 신청한 순서대로 방을 배정합니다.
-2. 고객은 투숙하기 원하는 방 번호를 제출합니다.
-3. 고객이 원하는 방이 비어 있다면 즉시 배정합니다.
-4. 고객이 원하는 방이 이미 배정되어 있으면 원하는 방보다 번호가 크면서 비어있는 방 중 가장 번호가 작은 방을 배정합니다.
-*/
+vector<long long> answer;
 unordered_map<ll, PLL> m;     //배정 받은 방번호, {배정 받은 사람, 다음 방 번호}
 ll find_room(ll i, ll key){
     if(m.find(key)==m.end()){
         m.insert({key,{i,key+1}});
+        answer.push_back(key);
         return key+1;
     }
     else{ 
@@ -29,24 +24,24 @@ ll find_room(ll i, ll key){
     }
 }
 vector<long long> solution(long long k, vector<long long> room_number) {
-    vector<long long> answer;
-    
     for(ll i=0; i<room_number.size(); i++){
         ll key = room_number[i];
         if(m.find(key)==m.end()){
             m.insert({key,{i,key+1}});
+            answer.push_back(key);
         }
         else{
             m[key].second = find_room(i,m[key].second);
         }
     }
-    vector<PLL> tmp;
-    for(auto res : m){
-        tmp.push_back({res.second.first, res.first});
-    }
-    sort(tmp.begin(), tmp.end());
-    for(auto ans : tmp)
-        answer.push_back(ans.second);    
+
+    // vector<PLL> tmp;
+    // for(auto res : m){
+    //     tmp.push_back({res.second.first, res.first});
+    // }
+    // sort(tmp.begin(), tmp.end());
+    // for(auto ans : tmp)
+    //     answer.push_back(ans.second);    
     return answer;
 }
 int main(){
