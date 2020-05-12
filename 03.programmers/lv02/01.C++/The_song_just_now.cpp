3bc418b44580d102598e4fe675ad8e4931f8db8a
WONILLISM <wonillism@gmail.com>
2020-05-12 16:39:32 +0900

    2020-05-12 algorithm study

--- 03.programmers/lv02/01.C++/The_song_just_now.cpp
+++ 03.programmers/lv02/01.C++/The_song_just_now.cpp
@@ -14,15 +14,20 @@ bool comp(PP a, PP b){
     else return a.melody.size()>b.melody.size();
 }
 string Replace(string s){
-    int n1 =s.find("C#");
-    int n2 =s.find("D#");
-    int n3 =s.find("F#");
-    int n4 =s.find("G#");
-    if(n1!=-1){s.replace(n1,s.size(),"H");}
-    if(n2!=-1){s.replace(n2,s.size(),"I");}
-    if(n3!=-1){s.replace(n3,s.size(),"J");}
-    if(n4!=-1){s.replace(n4,s.size(),"K");}
-    return s;
+    string ret = "";
+    for(int i=0; i<s.size()-1; i++){
+        if(s[i+1] != '#') ret+=s[i];
+        else{
+            if(s[i] == 'C') ret+='H';
+            else if(s[i] == 'D') ret+='I';
+            else if(s[i] == 'F') ret+='J';
+            else if(s[i] == 'G') ret+='K';
+            else if(s[i] == 'A') ret+='L';
+            i++;
+        }
+    }
+    if(s[s.size()-1]!='#')ret +=s[s.size()-1];
+    return ret;
 }
 PP split(string s,int index){
     PP ret;
@@ -62,17 +67,18 @@ string solution(string m, vector<string> musicinfos) {
     for(int i=0; i<musicinfos.size(); i++){
         string s = musicinfos[i];
         PP tmp = split(s,i);
-        int n = tmp.melody.find(m);
-        if(n!=-1)vss.push_back(tmp);
+        string play = tmp.melody;
+        auto itr = play.find(m);
+        if(itr != string::npos)vss.push_back(tmp);        
     }
 
     sort(vss.begin(), vss.end(),comp);
     if(vss.size()) return answer = vss[0].title;
-    else return answer = "None";
+    else return answer = "(None)";
 }
 int main(){
     //string m  = "ABCDEFG";
-    string m = "ABC#";
+    string m = "ABC#D#";
     //vector<string> musicinfos = {"12:00,12:14,HELLO,CDEFGAB","13:00,13:05,WORLD,ABCDEF"};
     vector<string> musicinfos = {"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"};	
     cout<<solution(m, musicinfos)<<endl;
