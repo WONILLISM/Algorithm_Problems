0c2cc039f31b0ff7d2b4792df3943868e2247715
WONILLISM <wonillism@gmail.com>
2020-05-13 21:25:48 +0900

    .

--- 03.programmers/lv02/02.Python/delete_couple.py
+++ 03.programmers/lv02/02.Python/delete_couple.py
@@ -2,15 +2,8 @@ def solution(s):
     answer = 0
     stack = []
     for c in s:
-        if len(stack):
-            if stack[-1]==c:
-                stack.pop()
-                answer +=2
-            else: stack.append(c)
+        if len(stack) and stack[-1]==c:
+            stack.pop()
         else: stack.append(c)
     answer = 0 if len(stack) else 1
-
     return answer
-
-input_s="baabaa"
-print(solution(input_s))
