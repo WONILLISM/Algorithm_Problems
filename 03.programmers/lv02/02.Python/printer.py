1f3eabec0e7e308e7b297a3d38ecf8c82c66f4d2
WONILLISM <wonillism@gmail.com>
2020-05-27 08:42:30 +0900

    2020-05-26 algorithm study

--- 03.programmers/lv02/02.Python/printer.py
+++ 03.programmers/lv02/02.Python/printer.py
@@ -1,6 +1,15 @@
 def solution(pri, loc):
     answer = 0
-
+    while len(pri):
+        M = max(pri)
+        cur = pri.pop(0)
+        if cur == M:
+            answer+=1
+            if loc == 0: break
+            else: loc -=1
+        else:
+            if loc == 0: loc = len(pri)
+            pri.append(cur)
     return answer
 
 input_pri =[1,1,9,1,1,1]
