9d8553df25ae1f60db195ca5a92c6d0f4107af7d
WONILLISM <wonillism@gmail.com>
2020-07-21 17:07:52 +0900

    2020-07-21 algorithm study

--- 03.programmers/lv02/01.C++/08_more_spicy.cpp
+++ 03.programmers/lv02/01.C++/08_more_spicy.cpp
@@ -13,7 +13,7 @@ int solution(vector<int> scoville, int K)
     priority_queue<int, vector<int>, greater<int>> pq_scoville(scoville.begin(), scoville.end());
     while (pq_scoville.top() < K)
     {
-        if (pq_scoville.size() == -1)
+        if (pq_scoville.size() == 1)
             return -1;
         a = pq_scoville.top();
         pq_scoville.pop();
@@ -30,3 +30,36 @@ int main()
     cout << solution(scoville, K) << endl;
     return 0;
 }
+
+// #include<iostream>
+// #include <string>
+// #include <vector>
+// #include<queue>
+// #define ll long long
+
+// using namespace std;
+
+// priority_queue<ll,vector<ll>, greater<ll>> pq;
+// int solution(vector<int> scoville, int K) {
+//     int answer = 0;
+//     int size = scoville.size();
+//     priority_queue <int, vector<int>, greater<int> > pq;
+//     for (int i = 0; i < scoville.size(); i++)
+//         pq.push(scoville[i]);
+//     if (pq.top() < K && size > 1) {
+//         while (pq.top() < K && size > 1) {
+//             int temp1 = pq.top();
+//             pq.pop();
+//             int temp2 = pq.top();
+//             pq.pop();
+//             int temp3 = temp1 + temp2 * 2;
+//             pq.push(temp3);
+//             answer++;
+//             size = pq.size();
+//         }
+//     }
+
+//     if (pq.top() < K)
+//         answer = -1;
+//     return answer;
+// }
