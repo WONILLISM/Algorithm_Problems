d06e8d035c3c0412206eacb4f5c14861abd79cd9
WONILLISM <wonillism@gmail.com>
2020-09-24 15:22:33 +0900

    2020-09-24 algorithm study

---
+++ 01.baekjoon/01.boj_step/01.C++/Step13/02_15650.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+
+using namespace std;
+
+int n, m;
+vector<int> stack;
+
+void solution(int cnt, int j){
+	if (cnt == m){
+		for (auto ans : stack)
+			cout<<ans<<" ";
+		cout<<endl;
+		return ;
+	}
+	for (int i = j; i <= n; i++){
+			stack.push_back(i);
+			solution(cnt + 1, i + 1);
+			stack.pop_back();
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	solution(0, 1);
+	
+	return 0;
+}
