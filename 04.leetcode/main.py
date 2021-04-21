fd6026cdc9df4c1db52ff1f029470596a2045ac8
Wonil Park <wopark@c6r8s6.42seoul.kr>
2021-04-21 13:39:04 +0900

    add Defanging an IP address

--- 04.leetcode/main.py
+++ 04.leetcode/main.py
@@ -1,4 +1,8 @@
 from RunningSumOf1dArray import Solution1480
+from DefangingAnIPAddress import Solution1108
 
 s1480 = Solution1480()
 print(s1480.runningSum([1,2,3,4]))
+
+s1108 = Solution1108()
+print(s1108.defangIPaddr("1.1.1.1"))
