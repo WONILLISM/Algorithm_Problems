7e84271caa76de8c59dffd491708fefbd7ce060f
WONILLISM <47653005+WONILLISM@users.noreply.github.com>
2021-04-30 15:07:51 +0900

    use int() base

--- 03.programmers/lv01/02.Python/trans_3.py
+++ 03.programmers/lv01/02.Python/trans_3.py
@@ -1,12 +1,23 @@
+#def solution(n):
+#	answer = 0
+#	tmp = ""
+#	while n:
+#		tmp += str(n%3)
+#		n//=3
+#	tmp = tmp.lstrip('0')
+#	for i, t in enumerate(tmp):
+#		print(answer)
+#		answer += int(t) * 3 ** (len(tmp) -(i + 1))
+
+#	return answer
+
+
+# https://codetorial.net/tips_and_examples/int_function.html
+# 파이썬 int()사용법
 def solution(n):
 	answer = 0
 	tmp = ""
 	while n:
 		tmp += str(n%3)
 		n//=3
-	tmp = tmp.lstrip('0')
-	for i, t in enumerate(tmp):
-		print(answer)
-		answer += int(t) * 3 ** (len(tmp) -(i + 1))
-
-	return answer
+	answer = int(tmp, 3)
