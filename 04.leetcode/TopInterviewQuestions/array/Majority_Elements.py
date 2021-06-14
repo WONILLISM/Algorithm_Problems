a1b00b2d94b49af104f8844e4995532bf53d989b
WONILLISM <47653005+WONILLISM@users.noreply.github.com>
2021-06-15 00:15:58 +0900

    del

--- 04.leetcode/TopInterviewQuestions/array/Majority_Elements.py
+++ 04.leetcode/TopInterviewQuestions/array/Majority_Elements.py
@@ -1,4 +1,21 @@
-class Solution169:
-	def majorityElement(self, nums: List[int]) -> int:
-		print("asdf")
+from typing import List
 
+class Solution:
+	def majorityElement(self, nums: List[int]) -> int:
+		d = {}
+		max_key = 0;
+		max_val = 0;
+		for num in nums:
+			if num not in d:
+				d[num] = 1
+			else:
+				d[num] += 1
+
+			if max_val < d[num]:
+				max_val = d[num]
+				max_key = num
+		return max_key
+
+sol = Solution()
+print(sol.majorityElement([3,2,3]))
+print(sol.majorityElement([2,2,1,1,1,2,2]))
