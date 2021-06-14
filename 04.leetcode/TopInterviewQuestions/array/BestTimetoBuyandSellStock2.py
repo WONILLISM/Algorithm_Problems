476b429728a289ed532a96c356f5ac9fee4cec99
WONILLISM <47653005+WONILLISM@users.noreply.github.com>
2021-06-15 00:16:16 +0900

    solved

---
+++ 04.leetcode/TopInterviewQuestions/array/BestTimetoBuyandSellStock2.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+	def maxProfit(self, prices: List[int]) -> int:
+		answer = 0;
+		for i in range(len(prices) - 1):
+			if prices[i + 1] > prices[i]:
+				answer += prices[i + 1] - prices[i]
+		return answer
+
+sol = Solution()
+print(sol.maxProfit([7,1,5,3,6,4]))
